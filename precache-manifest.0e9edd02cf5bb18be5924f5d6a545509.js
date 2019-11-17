9cd41c1cdf15af647475f6209423585544eb3468
andres-bernaus <andres.bernaus@hotmail.com>
2019-11-17 18:20:48 -0300

    Updates

---
+++ precache-manifest.0e9edd02cf5bb18be5924f5d6a545509.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0360f9b4f09628238912d97d214b955",
+    "url": "/paginaWeb/index.html"
+  },
+  {
+    "revision": "e7e710b53e8413dceaa7",
+    "url": "/paginaWeb/static/css/main.d8bdd235.chunk.css"
+  },
+  {
+    "revision": "40ecbd8ae731886603f2",
+    "url": "/paginaWeb/static/js/2.d1e61428.chunk.js"
+  },
+  {
+    "revision": "e7e710b53e8413dceaa7",
+    "url": "/paginaWeb/static/js/main.2e53c626.chunk.js"
+  },
+  {
+    "revision": "6378f98b261ebf981f5f",
+    "url": "/paginaWeb/static/js/runtime-main.461d9d92.js"
+  },
+  {
+    "revision": "e47f883c495360e9cd6cd38c27871495",
+    "url": "/paginaWeb/static/media/android-icon.e47f883c.png"
+  },
+  {
+    "revision": "63bb322c065f86c4f4c1fc181a2d9d6f",
+    "url": "/paginaWeb/static/media/background.63bb322c.jpeg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/paginaWeb/static/media/react-icon.04557c1b.png"
+  }
+]);
